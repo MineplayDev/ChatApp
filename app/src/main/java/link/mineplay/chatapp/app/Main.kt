1b38feeb3890555130c21c2780acaf4dce40797b
deinNachbar1 <74561856+deinnachbar1@users.noreply.github.com>
2021-12-26 22:00:36 +0100

    create layout

---
+++ app/src/main/java/link/mineplay/chatapp/app/Main.kt
@@ -0,0 +1,13 @@
+package link.mineplay.chatapp.app
+
+import android.app.Activity
+import android.os.Bundle
+
+class Main : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
--- app/src/test/java/link/mineplay/chatapp/app/ExampleUnitTest.kt
+++ app/src/test/java/link/mineplay/chatapp/app/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.chatapp
+package link.mineplay.chatapp.app
 
 import org.junit.Test
 
